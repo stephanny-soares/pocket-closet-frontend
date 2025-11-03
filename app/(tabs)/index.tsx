49573d5cf5a6722b4413351fac12ee77a294bac9
Stephanny Soares <stephannysoares.3@hotmail.com>
2025-11-03 13:40:10 +0100

    chore: base inicial de React Native + Expo + NativeWind

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-lg font-bold">
+        Hola NativeWind!
+      </Text>
+    </View>
+  );
+}
